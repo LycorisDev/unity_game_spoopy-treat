56af4e197ff9ffc15cda48dd3e3e13f1b238343a
Lycoris Bellua <lycoris.bellua@gmail.com>
2023-08-14 14:34:44 +0200

    Fix bounding area script

--- Assets/Scripts/Player/BoundingArea.cs
+++ Assets/Scripts/Player/BoundingArea.cs
@@ -6,16 +6,12 @@ public class BoundingArea : MonoBehaviour
 
     private void Awake()
     {
-        // Take the player's first position
-        _spawnPoint = GameObject.FindGameObjectWithTag("Player").transform.position;
-        // And add +2 to the y axis to make sure the spawn point is above ground
-        _spawnPoint += new Vector3(0f, 2f, 0f);
-        Debug.Log("[TODO] Fix script: BoundingArea - The spawn point is correct, but the object's position isn't updated");
+        _spawnPoint = transform.position;
     }
 
-    // If an object hits the bound, put it back to the spawn point
     private void OnTriggerExit(Collider other)
     {
-        other.transform.position = _spawnPoint;
+        if (other.tag == "InvisibleWall")
+            transform.position = _spawnPoint;
     }
 }
